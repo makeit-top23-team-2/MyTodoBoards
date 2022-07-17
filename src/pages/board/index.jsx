2d9e446f8231fe44fd35db9c7283c5b2d106795b
David Pineda <105688790+dspineda@users.noreply.github.com>
2022-07-17 17:32:21 -0500

    Feat/add the button add list (#19)
    * feat: junk file are deleted and add new routes
    
    * feat: Add the button -Add list- and config generally

--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -3,16 +3,26 @@ import  './mainboard.scss';
 
 const MainBoard= () => {
   return(
-    <div>
-        <header className="headerBoard">
-                <input className="nameBoard"  type="text"/>
-                <a  href="/"> <img className="iconsBoard" src="..\img\star-regular.png" alt=""/></a>
-                <a  href="/">Espacio de trabajo de Trello</a>   
-                <a  href="/"><img className="iconsBoard" src="..\img\users-solid.png" alt="" /> Workspace visible</a>
-                <a  href="/"><img className="iconsBoard" src="..\img\share-square-solid.png" alt="" />  Share</a>
-                <a  href="/"><img className="iconsBoard" src="..\img\filter-solid.png" alt="" />  Filter</a>
-                <a  href="/"><img className="iconsBoard" src="..\img\ellipsis-h-solid.png" alt="" />   Show menu</a>
-        </header>
+        <div>
+
+                <div className="mainBoard">
+                        <header className="headerBoard">
+                                <input className="nameBoard"  type="text" placeholder=" Tittle"/>
+                                <a  href="/"> <img className="iconsBoard" src="..\img\star-regular.png" alt=""/></a>
+                                <a  href="/">Espacio de trabajo de Trello</a>   
+                                <a  href="/"><img className="iconsBoard" src="..\img\users-solid.png" alt="" /> Workspace visible</a>
+                                <a  href="/"><img className="iconsBoard" src="..\img\share-square-solid.png" alt="" />  Share</a>
+                                <a  href="/"><img className="iconsBoard" src="..\img\filter-solid.png" alt="" />  Filter</a>
+                                <a  href="/"><img className="iconsBoard" src="..\img\ellipsis-h-solid.png" alt="" />   Show menu</a>
+                        </header>
+                </div>
+
+                <div className="workSpace">
+                        <div class="list">
+                                <input type="text" class="textContent buttonCard " placeholder="+ Add a list"/>
+                        </div>
+                </div>
+
 
         </div>
 )   
