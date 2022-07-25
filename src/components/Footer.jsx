ea6b8764fbe53a58a165e4738b4a1f8a9f21bf76
Mauro Arango <91640454+ffyto@users.noreply.github.com>
2022-07-25 15:30:45 +0200

    Feat/to do add columns (#30)
    * feat: first ToDo implementations
    
    * feat:add the function create columns with function create cards
    
    * Add auto-clean input for lists
    
    * feat: correct ToDo and general styles
    
    Co-authored-by: David Pineda <dspinedao@gmail.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,33 +2,33 @@ import {NavLink} from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <section class="footer_section">
-        <div class="footer_section__up">
-          <div class="footer__div">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Trello-logo-blue.svg/2560px-Trello-logo-blue.svg.png" alt="" class="footer__logo"/>
-            <div class="footer__a">
+    <footer className="footer">
+      <section className="footer_section">
+        <div className="footer_section__up">
+          <div className="footer__div">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Trello-logo-blue.svg/2560px-Trello-logo-blue.svg.png" alt="" className="footer__logo"/>
+            <div className="footer__a">
               <NavLink to = "/login">Login</NavLink>
             </div>
           </div>
         </div>
       </section>
-      <section class="footer__section">
-        <div class="footer_section__down">
-          <div class="footer__div__center">
-            <ul class="footer__ul">
-              <li class="footer__li"><a class="redes" href="#">Privacy Policy</a></li>
-              <li class="footer__li"><a class="redes" href="#">Terms</a></li>
-              <li class="footer__li"><a class="redes" href="#">Cookie Settings</a></li>
-              <li class="footer__li"><a class="redes" href="#">Copiright 2022 Atlassian</a></li>
+      <section className="footer__section">
+        <div className="footer_section__down">
+          <div className="footer__div__center">
+            <ul className="footer__ul">
+              <li className="footer__li"><a className="redes" href="#">Privacy Policy</a></li>
+              <li className="footer__li"><a className="redes" href="#">Terms</a></li>
+              <li className="footer__li"><a className="redes" href="#">Cookie Settings</a></li>
+              <li className="footer__li"><a className="redes" href="#">Copiright 2022 Atlassian</a></li>
             </ul>
           </div>
-          {/* <div class="footer__div__down">
-            <a class="redes" href="#"><i class="fa-brands fa-instagram"></i></a>
-            <a class="redes" href="#"><i class="fa-brands fa-facebook"></i></a>
-            <a class="redes" href="#"><i class="fa-brands fa-linkedin"></i></a>
-            <a class="redes" href="#"><i class="fa-brands fa-twitter"></i></a>
-            <a class="redes" href="#"><i class="fa-brands fa-youtube"></i></a>
+          {/* <div className="footer__div__down">
+            <a className="redes" href="#"><i className="fa-brands fa-instagram"></i></a>
+            <a className="redes" href="#"><i className="fa-brands fa-facebook"></i></a>
+            <a className="redes" href="#"><i className="fa-brands fa-linkedin"></i></a>
+            <a className="redes" href="#"><i className="fa-brands fa-twitter"></i></a>
+            <a className="redes" href="#"><i className="fa-brands fa-youtube"></i></a>
           </div> */}
         </div>
       </section>
