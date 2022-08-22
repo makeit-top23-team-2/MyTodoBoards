02256150bead3f31e26f5c195ec7385dc00c88dd
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-08-22 12:49:57 -0500

    feat: add ReactPortal

--- src/components/createBoard/index.jsx
+++ src/components/createBoard/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
+import { createPortal } from "react-dom";
 import BackgroundBoard from '../backgroundBoard';
 
-function CreateBoard({ modal, setModal }) {
+function CreateBoard({ isModalOpened, setIsModalOpened }) {
   const [task, setTask] = useState('');
-
-  const handleModal = () => {
-    setModal(!modal);
-  };
-
+  
+  const handleCloseModal = () => {
+    setIsModalOpened(false);
+  }
+  
   const handleInput = e => {
     setTask(e.target.value);
   };
@@ -18,101 +19,82 @@ function CreateBoard({ modal, setModal }) {
     setTask('');
   };
 
-  return (
+  return createPortal(
     <div>
-      {modal && (
-        <main className='board'>
-          <header className='board__header'>
-            <div className='board__header__div'>Create Board</div>
-            <button
-              type='button'
-              onClick={handleModal}
-              className='board__header__button'
-            >
-              <i className='fa-solid fa-xmark' />
-            </button>
-          </header>
+    
+      {isModalOpened && (
+        <div>  
+          <main className='board'>
+            <header className='board__header'>
+              <div className='board__header__div'>Create Board</div>
+              <button
+                type='button'
+                className='board__header__button'                
+                onClick={handleCloseModal}
+              >
+                <i className='fa-solid fa-xmark' />
+              </button>
+            </header>
 
-          <section className='board__section'>
-            <div className='board__section__div'>
-              <img
-                className='board__section__image'
-                src='..\img\image_boardPreview.jpg'
-                alt='image_board-preview'
-              />
-            </div>
-
-            <div className='board__section__background'>
-              <div>
-                <span className='board__section__background__label'>
-                  Background
-                </span>
-              </div>
-              <div>
-                <BackgroundBoard />
-              </div>
-            </div>
-
-            <form className='board__section__form' onSubmit={handleForm}>
-              <div>
-                <span className='board__section__label'>
-                  Board Title <span className='board__section__span'>*</span>
-                </span>
-                <input
-                  onChange={handleInput}
-                  className='board__section__input'
-                  type='text'
-                  value={task}
+            <section className='board__section'>
+              <div className='board__section__div'>
+                <img
+                  className='board__section__image'
+                  src='..\img\image_boardPreview.jpg'
+                  alt='image_board-preview'
                 />
-                <span className='board__section__span--2'>
-                  👋 Board title is required
-                </span>
-                <button
-                  type='button'
-                  className='board__section__button'
-                  disabled={task.length > 3 ? '' : 'disabled'}
-                >
-                  Create
-                </button>
               </div>
-            </form>
 
-            <div className='board__section__footer'>
-              <div className='board__section__text'>
-                By using images from Unsplash, you agree to their
-                <a
-                  className='board__section__a'
-                  href='https://unsplash.com/license'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  license
-                </a>
-                and
-                <a
-                  className='board__section__a'
-                  href='https://unsplash.com/terms'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Terms of Service
-                </a>
+              <div className='board__section__background'>
+                <div>
+                  <span className='board__section__background__label'>
+                    Background
+                  </span>
+                </div>
+                <div>
+                  <BackgroundBoard />
+                </div>
               </div>
-            </div>
-          </section>
-        </main>
-      )}
-    </div>
+
+              <form className='board__section__form' onSubmit={handleForm}>
+                <div>
+                  <span className='board__section__label'>
+                    Board Title <span className='board__section__span'>*</span>
+                  </span>
+                  <input
+                    onChange={handleInput}
+                    className='board__section__input'
+                    type='text'
+                    value={task}
+                  />
+                  <span className='board__section__span--2'>
+                    👋 Board title is required
+                  </span>
+                  <button
+                    type='button'
+                    className='board__section__button'
+                    disabled={task.length > 3 ? '' : 'disabled'}
+                  >
+                    Create
+                  </button>
+                </div>
+              </form>              
+            </section>
+          </main>
+        </div>
+        )}    
+    </div>,
+    document.getElementById('modal')
   );
 }
 
 CreateBoard.propTypes = {
-  modal: PropTypes.bool,
-  setModal: PropTypes.func,
+  isModalOpened: PropTypes.bool,
+  setIsModalOpened:  PropTypes.func,
 };
 CreateBoard.defaultProps = {
-  modal: false,
-  setModal: () => null,
+  isModalOpened: false,
+  setIsModalOpened: () => null,
 };
 
 export default CreateBoard;
