4fdbbaf0310a324ea4bd525c6f6a6b3ba9941e82
Mauro Arango <91640454+ffyto@users.noreply.github.com>
2022-08-24 13:10:40 -0500

    Update columns.js

--- src/services/columns.js
+++ src/services/columns.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function getColumns() {
   const response = await fetch(`${BASE_URL}/api/columns`);
