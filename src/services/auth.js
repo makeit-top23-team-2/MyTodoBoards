537081f735cf44694ece1d8ef43e0d920c90049f
Mauro Arango <91640454+ffyto@users.noreply.github.com>
2022-08-24 13:10:04 -0500

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function login(email, password) {
   const response = await fetch(`${BASE_URL}/auth/local/login`, {
