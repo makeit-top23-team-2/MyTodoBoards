6d8050f709256a65c7c6927b81d2af71b1a91eb3
Bryan <ee.mxtrd@gmail.com>
2022-09-08 17:18:34 -0500

    feat avatar button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,53 +2,14 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='footer'>
-      <section className='footer_section'>
-        <div className='footer_section__up'>
-          <div className='footer__div'>
-            <img
-              src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Trello-logo-blue.svg/2560px-Trello-logo-blue.svg.png'
-              alt=''
-              className='footer__logo'
-            />
-          </div>
-        </div>
-      </section>
-      <section className='footer__section'>
-        <div className='footer_section__down'>
-          <div className='footer__div__center'>
-            <ul className='footer__ul'>
-              <li className='footer__li'>
-                <a className='redes' href='*'>
-                  Privacy Policy
-                </a>
-              </li>
-              <li className='footer__li'>
-                <a className='redes' href='*'>
-                  Terms
-                </a>
-              </li>
-              <li className='footer__li'>
-                <a className='redes' href='*'>
-                  Cookie Settings
-                </a>
-              </li>
-              <li className='footer__li'>
-                <a className='redes' href='*'>
-                  Copiright 2022 Atlassian
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/* <div classNameName="footer__div__down">
-            <a classNameName="redes" href="#"><i classNameName="fa-brands fa-instagram"></i></a>
-            <a classNameName="redes" href="#"><i classNameName="fa-brands fa-facebook"></i></a>
-            <a classNameName="redes" href="#"><i classNameName="fa-brands fa-linkedin"></i></a>
-            <a classNameName="redes" href="#"><i classNameName="fa-brands fa-twitter"></i></a>
-            <a classNameName="redes" href="#"><i classNameName="fa-brands fa-youtube"></i></a>
-          </div> */}
-        </div>
-      </section>
+    <footer>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+        <path
+          fill='#0099ff'
+          fill-opacity='1'
+          d='M0,160L48,165.3C96,171,192,181,288,186.7C384,192,480,192,576,176C672,160,768,128,864,128C960,128,1056,160,1152,154.7C1248,149,1344,107,1392,85.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'
+        />
+      </svg>
     </footer>
   );
 }
